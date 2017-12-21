6d1f38317174b186ea6181e00b27377a32ef7986
zuixian <hejianqiong@hejianqiongdeMacBook-Pro.local>
2017-12-21 19:49:00 +0800

    添加hero

---
+++ Classes/Hero.cpp
@@ -0,0 +1,8 @@
+//
+//  Hero.cpp
+//  MyCppGame
+//
+//  Created by zuixian on 2017/12/20.
+//
+
+#include <stdio.h>
---
+++ Classes/Hero.h
@@ -0,0 +1,12 @@
+//
+//  Hero.h
+//  MyCppGame
+//
+//  Created by zuixian on 2017/12/20.
+//
+
+#ifndef Hero_h
+#define Hero_h
+
+
+#endif /* Hero_h */
