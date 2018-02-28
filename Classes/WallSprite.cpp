d36a8710244d942e3e1e0c9990e446d3a7a9b9d5
zuixian <hejianqiong@hejianqiongdeMacBook-Pro.local>
2018-02-28 20:53:09 +0800

    创建墙体，数据待调试。

---
+++ Classes/WallSprite.cpp
@@ -0,0 +1,8 @@
+//
+//  WallSprite.cpp
+//  MyCppGame
+//
+//  Created by zuixian on 2018/2/28.
+//
+
+#include <stdio.h>
---
+++ Classes/WallSprite.h
@@ -0,0 +1,12 @@
+//
+//  WallSprite.h
+//  MyCppGame
+//
+//  Created by zuixian on 2018/2/28.
+//
+
+#ifndef WallSprite_h
+#define WallSprite_h
+
+
+#endif /* WallSprite_h */
